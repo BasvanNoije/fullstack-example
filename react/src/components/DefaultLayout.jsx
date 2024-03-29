7c9612e5e6ab3ba4dec0bf4a958ec00544ba3109
basvannoije <basvnoye7@gmail.com>
2024-03-29 21:49:18 +0100

    Added support for deleting, adding and editing users

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -5,7 +5,7 @@ import axiosClient from "../axios-client";
 
 export default function DefaultLayout() {
 
-  const { user, token, setUser, setToken } = useStateContext();
+  const { user, token, notification, setNotification, setUser, setToken } = useStateContext();
 
   if (!token) {
     return <Navigate to="/login" />
@@ -48,6 +48,11 @@ export default function DefaultLayout() {
           <Outlet />
         </main>
       </div>
+      {notification &&
+        <div className="notification">
+          {notification}
+        </div>
+      }
     </div>
   )
 }
--- react/src/context/ContextProvider.jsx
+++ react/src/context/ContextProvider.jsx
@@ -3,14 +3,23 @@ import { createContext, useContext, useState } from "react";
 const StateContext = createContext({
   currentUser: null,
   token: null,
+  Notification: null,
   setUser: () => { },
   setToken: () => { },
+  setNotification: () => { }
 });
 
 export const ContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
-  // const [token, _setToken] = useState(123);
+  const [notification, _setNotification] = useState('')
+
+  const setNotification = (message) => {
+    _setNotification(message);
+    setTimeout(() => {
+      _setNotification('')
+    }, 5000)
+  }
 
   const setToken = (token) => {
     _setToken(token)
@@ -26,7 +35,9 @@ export const ContextProvider = ({ children }) => {
       user,
       token,
       setUser,
-      setToken
+      setToken,
+      notification,
+      setNotification,
     }}>
       {children}
     </StateContext.Provider>
